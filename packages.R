c40590c536439c90db101e8243d757e55a41c080
James Eddy <james.a.eddy@gmail.com>
2017-02-27 16:37:19 -0800

    initial commit with script for installing packages plus notebook chunks for retrieving, loading miRNA data

---
+++ packages.R
@@ -0,0 +1,17 @@
+# make sure SSL CA cert can be recognized by R in conda env
+if (!("CURL_CA_BUNDLE" %in% names(Sys.getenv()))) {
+    Sys.setenv(CURL_CA_BUNDLE = file.path(Sys.getenv("CONDA_PREFIX"), 
+                                          "ssl/cacert.pem"))
+}
+
+# install Synapse
+if (!require(synapseClient)) {
+    source("http://depot.sagebase.org/CRAN.R")
+    pkgInstall("synapseClient")
+}
+
+# install CRAN and Bioconductor packages
+install.packages("feather")
+install.packages("ggthemes")
+install.packages("ggbeeswarm")
+# BiocInstaller::biocLite(pkg_list, suppressUpdates = T, suppressAutoUpdate = T)
